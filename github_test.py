c99062f63b44081538330b2b792bcc9aab2a165e
Aaron <js1130103@ms.nnkieh.tn.edu.tw>
2026-06-06 11:41:39 +0800

    test_3

--- github_test.py
+++ github_test.py
@@ -1,5 +1,6 @@
 print("hello world")
-print("現在是版本1")
+print("現在是版本3")
+import random
 class Player:
   def __init__(self,name):#初始化固定寫法，第一個參數必為self，第二格才是真正輸入的參數
     self.name =name
@@ -8,3 +9,23 @@ class Player:
     self.atk =0
     self.defe= 50
     self.born =1
+  def training(self):
+    print("我要訓練")
+    a=random.randint(1,5)
+    self.power=self.power+a
+    return self.power
+
+  def attack(self):
+    b=random.randint(1,10)
+    self.atk=self.power+b
+    c=random.random()
+    if c<0.2:
+      print("爆擊")
+      self.atk=self.atk*2
+    else:
+      print("普通攻擊")
+
+
+
+
+    return self.atk
