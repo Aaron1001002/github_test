0f80e16b373c390aa9677ff16006722f5f8ba359
Aaron <js1130103@ms.nnkieh.tn.edu.tw>
2026-06-06 11:39:53 +0800

    test_2

--- github_test.py
+++ github_test.py
@@ -1,2 +1,10 @@
 print("hello world")
 print("現在是版本1")
+class Player:
+  def __init__(self,name):#初始化固定寫法，第一個參數必為self，第二格才是真正輸入的參數
+    self.name =name
+    self.hp =100
+    self.power =10
+    self.atk =0
+    self.defe= 50
+    self.born =1
